584026305cd28acac27605e6646269b199eb2e60
Stuyk <stuykgaming@gmail.com>
2019-06-30 18:07:46 -0600

    Reconnect Prevention

---
+++ server/modules/eventFunctions.mjs
@@ -0,0 +1,79 @@
+import * as alt from 'alt';
+import * as extended from 'server-extended'; //https://github.com/team-stuyk-alt-v/altV-Extended
+import * as chat from 'chat'; //https://github.com/team-stuyk-alt-v/altV-Chat-Extended
+import { skinList } from './skins.mjs';
+import * as utility from './utility.mjs';
+
+const spawnLocation = {x: -1136.07, y: -3044.18, z: 14.1};
+var disconnectedPlayers = new Map();
+
+export function playerFirstJoin(player) {
+    // Prevent reconnections to the server to some degree.
+    if (disconnectedPlayers.get(player.name) !== undefined) {
+        extended.SetupExportsForPlayer(player);
+        player.fadeScreen(true, 5000);
+        player.freeze(true);
+        disconnectedPlayers.set(player.name, Date.now() + 120000);
+        chat.send(player, `{FF0000} You recently disconnected. Please close your game and rejoin.`);
+        setTimeout(() => {
+            player.kick();
+        }, 15000);
+        return;
+    }
+
+    alt.emit('broadcastMessage', `{FFF000}${player.name}{FFFFFF} has joined the server.`);
+    utility.loadModelForPlayers(player);
+
+    SpawnPlayer(player); 
+}
+
+export function playerDisconnect(player) {
+    // Remove from dimension if they're in one.
+    if (player.currentDimension !== undefined) {
+        player.currentDimension.Remove(player);
+    }
+
+    // Disconnect the player for 25 seconds.
+    disconnectedPlayers.set(player.name, Date.now() + 120000);
+}
+
+export function checkDisconnects() {
+    disconnectedPlayers.forEach((time, name) => {
+        if (Date.now() < time)
+            return;
+
+        disconnectedPlayers.delete(name);
+        console.log(`===> ${name} is free to rejoin.`);
+    });
+}
+
+export function respawnPlayer(target, killer, weapon) {
+    target.showSubtitle("~r~You have died; you will be respawned shortly.", 5000);
+    var randomPosition = extended.RandomPosAround(spawnLocation, 50);
+    
+    var skin = target.model;
+    target.fadeScreen(true, 3000);
+    
+    setTimeout(() => {
+        target.model = skin;
+        target.spawn(randomPosition.x, randomPosition.y, randomPosition.z);
+        target.health = 200;
+        target.fadeScreen(false, 2000);
+    }, 4000);
+}
+
+function SpawnPlayer(player) {
+    var randomPosition = extended.RandomPosAround(spawnLocation, 30);
+    var randomModel = Math.floor(Math.random() * skinList.length);
+    player.model = alt.hash(skinList[randomModel]);
+    
+    // Wait to set player health.
+    setTimeout(() => {
+        player.pos = randomPosition;
+        player.health = 200;
+    }, 1000);
+    
+    // Setup for extended / chat
+    chat.setupPlayer(player);
+    extended.SetupExportsForPlayer(player);
+}
--- server/modules/events.mjs
+++ server/modules/events.mjs
@@ -4,66 +4,21 @@ import * as chat from 'chat'; //https://github.com/team-stuyk-alt-v/altV-Chat-Ex
 import { skinList } from './skins.mjs';
 import { CurrentDimensions } from './dimension.mjs';
 import * as utility from './utility.mjs';
+import * as eventFuncs from './eventFunctions.mjs';
 
-const spawnLocation = {x: -1136.07, y: -3044.18, z: 14.1};
+// Called when the player spawns.
+alt.on('playerConnect', eventFuncs.playerFirstJoin);
 
-// Called when the player connects.
-alt.on('playerConnect', (player) => {
-    alt.emit('broadcastMessage', `{FFF000}${player.name}{FFFFFF} has joined the server.`);
+// Called when the player disconnects.
+alt.on('playerDisconnect', eventFuncs.playerDisconnect);
 
-    // Spawns the player.
-    SpawnPlayer(player); 
-
-    // Update Player Models
-    UpdateCurrentPlayerModels(player);
-});
-
-alt.on('playerDisconnect', (player) => {
-    // Dimension Helper
-    if (player.currentDimension !== undefined) {
-        player.currentDimension.Remove(player);
-    }
-});
-
-function SpawnPlayer(player) {
-    var randomPosition = extended.RandomPosAround(spawnLocation, 30);
-    var randomModel = Math.floor(Math.random() * skinList.length);
-    player.model = alt.hash(skinList[randomModel]);
-    player.pos = randomPosition;
-    player.dimension = 1;
-    setTimeout(() => {
-        player.dimension = 0;
-        player.health = 200;
-    }, 1000);
-
-    // Setup for extended / chat
-    chat.setupPlayer(player);
-    extended.SetupExportsForPlayer(player);
-}
-
-function UpdateCurrentPlayerModels(player) {
-    alt.Player.all.forEach((target) => {
-        alt.emitClient(player, 'updateModel', target.model);
-        alt.emitClient(target, 'updateModel', player.model);
-    });
-}
+// Handle disconnected players.
+setInterval(eventFuncs.checkDisconnects, 5000);
 
 // Called when the player dies.
-alt.on('playerDeath', (target, killer, weapon) => {
-    target.showSubtitle("~r~You have died; you will be respawned shortly.", 5000);
-    var randomPosition = extended.RandomPosAround(spawnLocation, 50);
-    
-    var skin = target.model;
-    target.fadeScreen(true, 3000);
-    
-    setTimeout(() => {
-        target.model = skin;
-        target.spawn(randomPosition.x, randomPosition.y, randomPosition.z);
-        target.health = 200;
-        target.fadeScreen(false, 2000);
-    }, 4000);
-});
+alt.on('playerDeath', eventFuncs.respawnPlayer);
 
+// Called when the player kill themself.
 alt.onClient('killSelf', (player) => {
     player.health = 0;
 });
